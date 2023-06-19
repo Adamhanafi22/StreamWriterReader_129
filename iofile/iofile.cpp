cbbb594d4a0721ee84e348887aa3f9662760f243
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-06-19 15:25:51 +0700

    Add project files.

---
+++ iofile/iofile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string baris;
+
+	//memasukan file spesifik
+	//string namafile;
+	//cout << "masukan namafile : ";
+	//cin >> namafile;
+
+	//membuka file dalam mode menulis
+
+}
