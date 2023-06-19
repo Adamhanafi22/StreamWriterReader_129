47ef6f91343bd230e8815a708be943c581ccf2d8
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-06-19 15:55:06 +0700

    melakukan perulangan

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -27,5 +27,17 @@ int main()
 	outfile.close();
 
 	ifstream infile;
+	infile.open(namafile + ".txt", ios::in);
+
+	cout << endl << ">= membuka dan membaca file" << endl;
+	if (infile.is_open())
+	{
+		while (getline(infile, baris))
+		{
+			cout << baris << '\n';
+
+		}
+
+	}
 
 } 
