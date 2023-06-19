f279043bbf9e9e62f0df425f81f957162cb6efd4
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-06-19 15:51:39 +0700

    unlimitid loop  untuk menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -5,9 +5,24 @@ using namespace std;
 
 int main()
 {
+	string baris;
+
 	string namafile;
 	cout << "masukan namafile : ";
 	cin >> namafile;
 
+	ofstream outfile;
+	outfile.open(namafile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q'\ untuk keluar" << endl;
+
+
+	while (true) {
+		cout << "_";
+		getline(cin, baris);
+		if (baris == "q")break;
+		outfile << baris << endl;
+
+	}
 
 } 
